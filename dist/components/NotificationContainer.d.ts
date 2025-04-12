c7c51b4a97479e1220c03af966c90c7b2788cd92
natewood2 <nathanwood.sfg@gmail.com>
2025-04-12 16:47:20 -0500

    Reset Git tracking and properly ignore node_modules

--- dist/components/NotificationContainer.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { NotificationItem } from '../types';
-interface NotificationContainerProps {
-    notifications: NotificationItem[];
-    onClose: (id: string) => void;
-}
-declare const NotificationContainer: React.FC<NotificationContainerProps>;
-export default NotificationContainer;
